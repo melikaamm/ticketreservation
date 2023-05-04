a26e371f1e78fd5d9eab05b8e09d9c9dc5633561
melikaamm <113208136+melikaamm@users.noreply.github.com>
2023-05-04 10:38:26 +0200

    Initial commit

---
+++ src/test/java/com/example/ticketreservation/TicketreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketreservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
